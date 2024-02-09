48af35fb014bb094520bbb4de3aaeeb2aa2b3df7
curtisjesse <curtisjesse487@gmail.com>
2024-02-09 11:49:24 +0300

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,51 @@
 from django.shortcuts import render, redirect
+# from django.contrib.auth.models import User, auth\
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User, auth
 
 def register(request):
-    return render(request, 'register.html')
-
-def sign_up(request):
     
     if request.method == 'POST':
+            
         first_name = request.POST['first_name']
         second_name = request.POST['second_name']
-        username = request.POST['username']
+        username = request.POST['user_name']
         email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
+        password1 = request.POST['password']
+        password2 = request.POST['confirm_password']
         
-        user = User.objects.create_user(username=username, password=password1, email=email, first_name= first_name, second_name=second_name)
-        user.save();
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name= first_name)
+        user.save()
         print('user created')
         return redirect('/')
-    else:      
-        return render(request, 'sign_up.html')
+    else: 
+         return render(request, 'register.html')
+
+def sign_in(request):
+    
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        password = request.POST['password']
+        
+        try:
+            user= User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'username does not exist!') 
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                messages.success(request, 'Logged in succesfully')
+                return redirect('/')
+            else:
+                messages.error(request, 'Please activate your account')
+                return redirect('/') 
+        else:
+                messages.error(request, 'Wrong password')
+                return redirect('/') 
+            
+    return render(request, 'sign_up.html')
 # Create your views here.
--- myweb/urls.py
+++ myweb/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('web/',include('web.urls')),
+    path('',include('web.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     
